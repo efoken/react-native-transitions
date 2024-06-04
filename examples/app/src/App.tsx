081df195585bf12ea75f530729fd04c7122212d6
Eike Foken <eike.foken@ibmix.de>
2024-06-04 12:59:47 +0200

    chore: setup Vite project example

--- examples/app/src/App.tsx
+++ examples/app/src/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { StrictMode, useState } from "react";
 import { Animated, StyleSheet, Text, View } from "react-native";
 import { Fade, Grow, Slide, Zoom } from "react-native-transitions";
 
@@ -26,25 +26,27 @@ export default function App() {
   const [checked, setChecked] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <Text onPress={() => setChecked((prevChecked) => !prevChecked)}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <View style={styles.boxGrid}>
-        <Grow in={checked}>
-          <Animated.View style={styles.box} />
-        </Grow>
-        <Fade in={checked}>
-          <Animated.View style={styles.box} />
-        </Fade>
-        <Zoom in={checked} onExited={() => console.log("onZoomExit")}>
-          <Animated.View style={styles.box} />
-        </Zoom>
-        <Slide in={checked} onExited={() => console.log("onSlideExit")}>
-          <Animated.View style={styles.box} />
-        </Slide>
+    <StrictMode>
+      <View style={styles.container}>
+        <Text onPress={() => setChecked((prevChecked) => !prevChecked)}>
+          Open up App.tsx to start working on your app!
+        </Text>
+        <View style={styles.boxGrid}>
+          <Grow in={checked}>
+            <Animated.View style={styles.box} />
+          </Grow>
+          <Fade in={checked}>
+            <Animated.View style={styles.box} />
+          </Fade>
+          <Zoom in={checked}>
+            <Animated.View style={styles.box} />
+          </Zoom>
+          <Slide in={checked}>
+            <Animated.View style={styles.box} />
+          </Slide>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </StrictMode>
   );
 }
