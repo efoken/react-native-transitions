dd2ec0a207989bf4c453ee2431ad60a76e85ec24
Eike Foken <eike.foken@ibmix.de>
2025-02-11 10:08:15 +0100

    chore: improve vite-project example

--- examples/vite-project/src/App.tsx
+++ examples/vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Fade } from "react-native-transitions";
+import { Fade, Slide, Zoom } from "react-native-transitions";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
@@ -11,10 +11,14 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <Slide in={count % 2 === 0}>
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </Slide>
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <Zoom in={count % 2 === 0}>
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </Zoom>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,7 +26,7 @@ function App() {
         <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
           count is {count}
         </button>
-        <Fade appear in>
+        <Fade in={count % 2 === 0}>
           <p>
             Edit <code>src/App.tsx</code> and save to test HMR
           </p>
